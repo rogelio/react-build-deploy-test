4d8deee065509a5aae9598628af58b0bd558a84d
rogelio <rogelio@demilane.com>
2022-12-14 03:24:34 +0800

    update

---
+++ src/Content.js
@@ -0,0 +1,17 @@
+const Content = () => {
+    const handleNameChange = () => {
+        const names = ['Alice', 'Bob', 'Chris', 'Dave', 'Eli', 'John', 'Mark'];
+        const int = Math.floor(Math.random() * names.length);
+    
+        return names[int];
+    }
+    return (
+        <main>
+            <p>
+                Hello {handleNameChange()}
+            </p>
+        </main>
+    )
+}
+
+export default Content
