b78cbad6bd8484232dc0a5f1ceb119ae660ed24a
rogelio <rogelio@demilane.com>
2022-12-14 05:16:58 +0800

    update

--- src/Content.js
+++ src/Content.js
@@ -5,11 +5,27 @@ const Content = () => {
     
         return names[int];
     }
+
+    const handleClick = () => {
+        console.log('You clicked it');
+    }
+
+    const handleClick2 = (name) => {
+        console.log(`${name} was clicked.`);
+    }
+
+    const handleClick3 = (e) => {
+        console.log(e.target.innerText);
+    }
+
     return (
         <main>
-            <p>
+            <p onDoubleClick={handleClick}>
                 Hello {handleNameChange()}.
             </p>
+            <button onClick={handleClick}>Click it</button>
+            <button onClick={() => handleClick2(handleNameChange())}>Name click it</button>
+            <button onClick={(e) => handleClick3(e)}>Event click it</button>
         </main>
     )
 }
